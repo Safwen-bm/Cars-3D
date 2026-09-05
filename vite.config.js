73b3c5f735fe359302dece71084e4f94cb7b9a59
Safwen-bm <safwenbenmabrouk@gmail.com>
2026-09-05 01:10:11 +0100

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "Cars-3D",
+  base: "/Cars-3D/",
 });
